80ccf352f450d25673102dad3841a37d115f623b
Christopher Johnstone <meson800@gmail.com>
2015-12-29 17:44:17 -0500

    Added code to derive shared key

--- noise/Crypto.cpp
+++ noise/Crypto.cpp
@@ -10,6 +10,8 @@ namespace openssl {
 #include <openssl/opensslconf.h>
 #include <openssl/conf.h>
 #include <openssl/err.h>
+#include <openssl/ec.h>
+#include <openssl/sha.h>
 
 }
 
@@ -30,6 +32,21 @@ Crypto::Crypto()
 		throw OpensslException("Can't initalize key generation");
 	if (!openssl::EVP_PKEY_CTX_set_rsa_keygen_bits(keyContext, 4096))
 		throw OpensslException("Can't set number of bits in RSA key");
+
+	//set up ephemeral key generation
+	if (NULL == (ephemeralParamContext = openssl::EVP_PKEY_CTX_new_id(EVP_PKEY_EC, NULL)))
+		throw OpensslException("Can't create new ephemeral parameter context");
+	if (1 != openssl::EVP_PKEY_paramgen_init(ephemeralParamContext))
+		throw OpensslException("Can't init ephemeral parameter context");
+	if (1 != openssl::EVP_PKEY_CTX_set_ec_paramgen_curve_nid(ephemeralParamContext, NID_X9_62_prime256v1))
+		throw OpensslException("Can't set ephemeral parameter curve");
+	if (!openssl::EVP_PKEY_paramgen(ephemeralParamContext, &ephemeralKeyParams))
+		throw OpensslException("Can't generate ephemeral key generation params");
+	if (NULL == (ephemeralKeyContext = openssl::EVP_PKEY_CTX_new(ephemeralKeyParams, NULL)))
+		throw OpensslException("Can't generate ephemeral key generation context");
+	if (1 != openssl::EVP_PKEY_keygen_init(ephemeralKeyContext))
+		throw OpensslException("Can't init ephemeral key generation context");
+
 	Log::writeToLog(Log::INFO, "Done initalizing crypto");
 }
 
@@ -37,6 +54,9 @@ Crypto::~Crypto()
 {
 	//free contexts
 	openssl::EVP_PKEY_CTX_free(keyContext);
+	openssl::EVP_PKEY_CTX_free(ephemeralParamContext);
+	openssl::EVP_PKEY_CTX_free(ephemeralParamContext);
+	openssl::EVP_PKEY_free(ephemeralKeyParams);
 }
 
 void Crypto::generateKeypair(openssl::EVP_PKEY ** key)
@@ -46,6 +66,15 @@ void Crypto::generateKeypair(openssl::EVP_PKEY ** key)
 		throw KeyGenerationException("Can't generate keypair, no key context");
 	if (!openssl::EVP_PKEY_keygen(keyContext, key))
 		throw KeyGenerationException("Key generation failed");
+}
+
+void Crypto::generateEphemeralKeypair(openssl::EVP_PKEY ** key)
+{
+	Log::writeToLog(Log::INFO, "Generating ephemeral EC keypair");
+	if (!ephemeralKeyContext)
+		throw KeyGenerationException("Can't generate ephemeral EC keypair, no context");
+	if (!openssl::EVP_PKEY_keygen(ephemeralKeyContext, key))
+		throw KeyGenerationException("Ephemeral key generation failed");
 }
 
 std::vector<unsigned char> Crypto::signMessage(openssl::EVP_PKEY * key, const std::vector<unsigned char>& message)
@@ -96,4 +125,41 @@ bool Crypto::verifySignature(openssl::EVP_PKEY * key, const std::vector<unsigned
 		return true;
 	}
 	return false;
+}
+void Crypto::deriveSharedKey(openssl::EVP_PKEY * key, openssl::EVP_PKEY * otherKey, 
+	std::vector<unsigned char>& sharedKeyData, std::vector<unsigned char>& sharedIv)
+{
+	openssl::EVP_PKEY_CTX* derivationContext = 0;
+	//start derivation context
+	if (NULL == (derivationContext = openssl::EVP_PKEY_CTX_new(key,NULL)))
+		throw OpensslException("Can't create key derivation context");
+	if (1 != openssl::EVP_PKEY_derive_init(derivationContext))
+		throw OpensslException("Can't init key derivation context");
+	//set other key
+	if (1 != openssl::EVP_PKEY_derive_set_peer(derivationContext, otherKey))
+		throw OpensslException("Can't set other peer key in key derivation");
+
+	//determine shared secret length
+	unsigned int secretSize = 0;
+	if (1 != openssl::EVP_PKEY_derive(derivationContext, NULL, &secretSize))
+		throw OpensslException("Can't derive shared secret");
+	//create temp buffer
+	unsigned char* tempBuffer = new unsigned char[secretSize];
+
+	if (1 != openssl::EVP_PKEY_derive(derivationContext, tempBuffer, &secretSize))
+		throw OpensslException("Can't derive shared secret");
+
+	std::vector<unsigned char> sharedSecret = std::vector<unsigned char>(tempBuffer, tempBuffer + secretSize);
+	//deallocate stuff
+	EVP_PKEY_CTX_free(derivationContext);
+	EVP_PKEY_free(key);
+	EVP_PKEY_free(otherKey);
+
+	//Hash the shared secret to derive a secure key
+	unsigned char* sharedKey = new unsigned char[SHA512_DIGEST_LENGTH];
+	openssl::SHA512(tempBuffer, secretSize, sharedKey);
+	sharedKeyData = std::vector<unsigned char>(sharedKey, sharedKey + 32);
+	sharedIv = std::vector<unsigned char>(sharedKey + 32, sharedKey + 32 + 16);
+	delete[](tempBuffer);
+	delete[](sharedKey);
 }
--- noise/Crypto.h
+++ noise/Crypto.h
@@ -18,9 +18,17 @@ public:
 	Crypto();
 	~Crypto();
 	void generateKeypair(openssl::EVP_PKEY** key);
+	void generateEphemeralKeypair(openssl::EVP_PKEY** key);
 	std::vector<unsigned char> signMessage(openssl::EVP_PKEY* key, const std::vector<unsigned char>& message);
 	bool verifySignature(openssl::EVP_PKEY* key, const std::vector<unsigned char>& message, const std::vector<unsigned char>& signature);
+	//Derive a shared key and IV based on two ephemeral keys. This function deallocates the keys afterwards
+	void deriveSharedKey(openssl::EVP_PKEY * key, openssl::EVP_PKEY * otherKey,
+		std::vector<unsigned char>& sharedKeyData, std::vector<unsigned char>& sharedIv);
 
 private:
 	openssl::EVP_PKEY_CTX* keyContext;
+
+	openssl::EVP_PKEY_CTX* ephemeralParamContext;
+	openssl::EVP_PKEY_CTX* ephemeralKeyContext;
+	openssl::EVP_PKEY* ephemeralKeyParams;
 };
