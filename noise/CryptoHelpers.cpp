a7df283e559f4280d09739c4d91be8f53398262c
Christopher Johnstone <meson800@gmail.com>
2015-12-29 18:43:01 -0500

    Added code to (de)seralize EC keys

--- noise/CryptoHelpers.cpp
+++ noise/CryptoHelpers.cpp
@@ -5,6 +5,7 @@
 
 namespace openssl {
 #include <openssl/evp.h>
+#include <openssl/ec.h>
 #include <openssl/rsa.h>
 #include <openssl/x509.h>
 }
@@ -50,4 +51,68 @@ openssl::EVP_PKEY * CryptoHelpers::bytesToOslPublicKey(const std::vector<unsigne
 		throw KeyConversionException("Failed to convert RSA key into EVP_PKEY");
 
 	return key;
+}
+
+std::vector<unsigned char> CryptoHelpers::ecPublicKeyToBytes(openssl::EVP_PKEY * key)
+{
+	Log::writeToLog(Log::L_DEBUG, "Converting ephemeral key to bytes");
+	openssl::EC_KEY* ecKey = openssl::EVP_PKEY_get1_EC_KEY(key);
+	if (ecKey == 0)
+		throw KeyConversionException("Couldn't extract ellptical curve key out of given key");
+
+	const openssl::EC_POINT* ecPoint = openssl::EC_KEY_get0_public_key(ecKey);
+	if (ecPoint == 0)
+		throw KeyConversionException("Couldn't extract a curve point of of EC key");
+
+	unsigned int bufferSize = openssl::EC_POINT_point2oct(openssl::EC_KEY_get0_group(ecKey), 
+		ecPoint, openssl::POINT_CONVERSION_COMPRESSED, NULL, 0,NULL);
+	unsigned char* tempBuffer = new unsigned char[bufferSize];
+
+	openssl::EC_POINT_point2oct(openssl::EC_KEY_get0_group(ecKey),
+		ecPoint, openssl::POINT_CONVERSION_COMPRESSED, tempBuffer, bufferSize, NULL);
+
+	std::vector<unsigned char> result = std::vector<unsigned char>(tempBuffer, tempBuffer + bufferSize);
+
+	delete[](tempBuffer);
+	openssl::EC_KEY_free(ecKey);
+	
+	return result;
+}
+
+openssl::EVP_PKEY * CryptoHelpers::bytesToEcPublicKey(const std::vector<unsigned char>& bytes)
+{
+	Log::writeToLog(Log::L_DEBUG, "Converting bytes to a EC key");
+
+	//get group from named curve
+	openssl::EC_GROUP* group = 0;
+	if (NULL == (group = openssl::EC_GROUP_new_by_curve_name(NID_X9_62_prime256v1)))
+		throw OpensslException("Couldn't get group from named curve");
+
+	openssl::EC_POINT* point = 0;
+	if (NULL == (point = openssl::EC_POINT_new(group)))
+		throw OpensslException("Couldn't init point from group");
+	if (1 != openssl::EC_POINT_oct2point(group, point, bytes.data(), bytes.size(), NULL))
+		throw OpensslException("Couldn't extract point from bytes");
+	openssl::EC_KEY* key = 0;
+
+	if (NULL == (key = openssl::EC_KEY_new()))
+		throw OpensslException("Couldn't init new ec key");
+	//generate key
+	if (1 != openssl::EC_KEY_set_public_key(key, point))
+		throw OpensslException("Couldn't create EC key from point");
+
+	//and finally generate EVP_PKEY
+	openssl::EVP_PKEY* finalKey = 0;
+	if (NULL == (finalKey = openssl::EVP_PKEY_new()))
+		throw OpensslException("Couldn't init new PKEY");
+	if (1 != openssl::EVP_PKEY_set1_EC_KEY(finalKey, key))
+		throw OpensslException("Couldn't generate PKEY from EC key");
+
+
+	//cleanup
+	openssl::EC_GROUP_free(group);
+	openssl::EC_POINT_free(point);
+	openssl::EC_KEY_free(key);
+
+	return finalKey;
 }
--- noise/CryptoHelpers.h
+++ noise/CryptoHelpers.h
@@ -11,4 +11,7 @@ struct CryptoHelpers
 {
 	static std::vector<unsigned char> oslPublicKeyToBytes(openssl::EVP_PKEY* key);
 	static openssl::EVP_PKEY* bytesToOslPublicKey(const std::vector<unsigned char>& bytes);
+
+	static std::vector<unsigned char> ecPublicKeyToBytes(openssl::EVP_PKEY* key);
+	static openssl::EVP_PKEY* bytesToEcPublicKey(const std::vector<unsigned char>& bytes);
 };
