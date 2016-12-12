0d9ae5e4784543a0a41e11dbdf85330d8fd7a01e
Christopher Johnstone <meson800@gmail.com>
2016-12-12 14:51:15 -0800

    Added trying to connect to directory server on localhost

--- noise/Network.cpp
+++ noise/Network.cpp
@@ -53,6 +53,7 @@ void Network::startNode()
 	Log::writeToLog(Log::INFO, "Connecting to directory server");
 	ourNode->AttachPlugin(&natClient);
 	ourNode->Connect("titanic.caltech.edu", DIRECTORY_SERVER_PORT, 0, 0);
+	ourNode->Connect("localhost", DIRECTORY_SERVER_PORT, 0, 0);
 
 	Log::writeToLog(Log::L_DEBUG, "Advertising node...");
 	mux.unlock();
